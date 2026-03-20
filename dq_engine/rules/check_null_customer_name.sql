be2ea021193cf8d63fe6ae37ad8b23c2e62aea81
Anthony <aamabile001@gmail.com>
2026-03-20 09:26:46 -0400

    added is active logic

--- dq_engine/rules/check_null_customer_name.sql
+++ dq_engine/rules/check_null_customer_name.sql
@@ -1,7 +1,7 @@
 -- rule: check null for null cusotmer_name values
 -- purpose: ensure customer_name is always populated for analytics
 
-SELECT
-    count(*) as null_customer_name_count
+SELECT count(*) as null_customer_name_count
 FROM customers
-WHERE customer_name IS NULL;
+WHERE customer_name IS NULL
+and is_active = 1;
